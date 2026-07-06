dbdc8ee55321ca909abb349a641d2eb55268eb70
AbinayaSelvam <sabinaya352@gmail.com>
2026-07-06 15:45:56 +0530

    Implemented the fixed enums for specifications and shift

--- app/src/main/java/org/examples/helper/ScannerHelper.java
+++ app/src/main/java/org/examples/helper/ScannerHelper.java
@@ -35,5 +35,30 @@ public class ScannerHelper {
 
             System.out.println("Input cannot be empty.");
         }
+
+    }
+    // Generic Enum Reader
+    public static <T extends Enum<T>> T readEnumChoice(
+            String title,
+            T[] values) {
+
+        while (true) {
+
+            System.out.println("\n" + title);
+
+            for (int i = 0; i < values.length; i++) {
+
+                System.out.println((i + 1) + ". " + values[i]);
+            }
+
+            int choice = readInt("Enter Choice : ");
+
+            if (choice >= 1 && choice <= values.length) {
+
+                return values[choice - 1];
+            }
+
+            System.out.println("Invalid Choice.");
+        }
     }
 }
