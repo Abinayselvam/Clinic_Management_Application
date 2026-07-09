c5224cffcc0dc2bf3381d38d7e79bebcde8dee0e
AbinayaSelvam <sabinaya352@gmail.com>
2026-07-09 10:12:57 +0530

    Implemented the log4j dependency

--- app/src/main/java/org/examples/helper/AuditLogger.java
+++ app/src/main/java/org/examples/helper/AuditLogger.java
@@ -1,18 +1,18 @@
-package org.examples.helper;
-
-import org.examples.model.AuditLog;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.List;
-
-public class AuditLogger {
-   private static final List<AuditLog> logs =new ArrayList<>();
-   public static void  log(String level, String message) {
-       DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd hh:mm:ss");
-       logs.add(new AuditLog( LocalDateTime.now().format(dtf), level, message));
-   }
-   public static List<AuditLog> getLogs() {
-       return logs;
-   }
-}
+//package org.examples.helper;
+//
+//import org.examples.model.AuditLog;
+//import java.time.LocalDateTime;
+//import java.time.format.DateTimeFormatter;
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//public class AuditLogger {
+//   private static final List<AuditLog> logs =new ArrayList<>();
+//   public static void  log(String level, String message) {
+//       DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd hh:mm:ss");
+//       logs.add(new AuditLog( LocalDateTime.now().format(dtf), level, message));
+//   }
+//   public static List<AuditLog> getLogs() {
+//       return logs;
+//   }
+//}
--- app/src/main/java/org/examples/model/AuditLog.java
+++ app/src/main/java/org/examples/model/AuditLog.java
@@ -1,27 +1,26 @@
-package org.examples.model;
-
-import java.time.LocalDateTime;
-
-public class AuditLog {
-    private String timestamp;
-    private String level;
-    private String message;
-    public AuditLog(String  timestamp, String level, String message) {
-        this.timestamp = timestamp;
-        this.level = level;
-        this.message = message;
-    }
-    public String  getTimestamp() {
-        return timestamp;
-    }
-    public String getLevel() {
-        return level;
-    }
-    public String getMessage() {
-        return message;
-    }
-    @Override
-    public String toString() {
-        return String.format("%-22s | %-8s | %s", timestamp, level, message);
-    }
-}
+//package org.examples.model;
+//
+//
+//public class AuditLog {
+//    private String timestamp;
+//    private String level;
+//    private String message;
+//    public AuditLog(String  timestamp, String level, String message) {
+//        this.timestamp = timestamp;
+//        this.level = level;
+//        this.message = message;
+//    }
+//    public String  getTimestamp() {
+//        return timestamp;
+//    }
+//    public String getLevel() {
+//        return level;
+//    }
+//    public String getMessage() {
+//        return message;
+//    }
+//    @Override
+//    public String toString() {
+//        return String.format("%-22s | %-8s | %s", timestamp, level, message);
+//    }
+//}
